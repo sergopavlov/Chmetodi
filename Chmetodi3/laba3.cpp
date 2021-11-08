596f12445dc928e5a59e41496df1e76d75d3448a
Сергей Павлов <s.pavlov.2019@azure.nstu.ru>
2021-11-08 22:39:16 +0700

    ни сделал нихуя

---
+++ Chmetodi3/laba3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "laba3.h"
+
+int main()
+{
+   int* ia, * ja;
+
+}
+
+void MSG(int* ia, int* ja, int n, real* al, real* di, real* x, real* b)
+{
+
+}
---
+++ Chmetodi3/laba3.h
@@ -0,0 +1,4 @@
+#pragma once
+typedef  double real;
+
+void MSG(int* ia, int* ja, int n, real* al, real* di, real* x, real* b);
